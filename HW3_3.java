42ac47ae292261c009857276e416d8fa79398d1b
Marina <petrukmn@yandex.ru>
2020-03-10 11:27:12 +0300

    HomeWork #3

---
+++ HW3_3.java
@@ -0,0 +1,9 @@
+package KompjuterIya.HomeWork;
+
+public class HW3_3 {
+    public static void main(String[] args) {
+        int i=10_000;
+        int sum = i*(i+1)/2;
+        System.out.print(sum);
+    }
+}
